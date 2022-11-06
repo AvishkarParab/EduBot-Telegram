a4f244ebb1c7519abecb55e327f62bd8a820a954
Avishkar Parab <aviparab72733@gmail.com>
2022-11-06 13:42:06 +0530

    please  do

--- index.js
+++ index.js
@@ -43,11 +43,10 @@ telebot.hears('teach', async (ctx) => {
 telebot.action('Scratch', async (ctx) => {
     ctx.telegram.sendMessage(ctx.chat.id, "You selected Scratch")
     let courseObj = await getCourses()
-    let dataState = courseObj.map((elem) => {
-        if (elem.cname === "Scratch")
-            return elem.id
-        else
-            return false;
+    let dataState
+    courseObj.map((elem) => {
+        if (elem.cname === "Kojo")
+            dataState =  elem.id
     })
     let i = 0
     if (dataState) {
@@ -133,11 +132,103 @@ telebot.action('Scratch', async (ctx) => {
 
 });
 
+telebot.action('Kojo', async (ctx) => {
+    ctx.telegram.sendMessage(ctx.chat.id, "You selected KOJO")
+    let courseObj = await getCourses()
+    let dataState 
+    courseObj.map((elem) => {
+        if (elem.cname === "Kojo")
+            dataState =  elem.id
+    })
+    let i = 0
+
+    if (dataState) {
+        //gets module for course
+        let moduleResponse = await getModule(dataState)
+        //if module type is video
+        if (moduleResponse[i].mtype === "video")
+            ctx.telegram.sendVideo(ctx.chat.id, "http://techslides.com/demos/sample-videos/small.mp4")
+
+        //if module type is image
+        else if (moduleResponse[i].mtype === "image")
+            ctx.telegram.sendPhoto(ctx.chat.id, moduleResponse[i].image)
+
+        ctx.telegram.sendMessage(ctx.chat.id, moduleResponse[i].question, {
+            reply_markup: {
+                inline_keyboard: [
+                    [{
+                        'text': moduleResponse[i].option1,
+                        'callback_data': (moduleResponse[i].correctopt === "1") ? "yes" : "no"
+                    }],
+                    [{
+                        'text': moduleResponse[i].option2,
+                        'callback_data': (moduleResponse[i].correctopt === "2") ? "yes" : "no"
+                    }],
+                    [{
+                        'text': moduleResponse[i].option3 ? moduleResponse[i].option3 : "",
+                        'callback_data': (moduleResponse[i].correctopt === "3") ? "yes" : "no"
+                    }],
+                    [{
+                        'text': moduleResponse[i].option4 ? moduleResponse[i].option4 : "",
+                        'callback_data': (moduleResponse[i].correctopt === "4") ? "yes" : "no"
+                    }],
+                ]
+            }
+        })
+        telebot.action("yes", async (ctx) => {
+            if (i === moduleResponse.length - 1)
+                ctx.telegram.sendMessage(ctx.chat.id, "Your Course is completed")
+            else {
+                i++;
+                //if module type is video
+                if (moduleResponse[i].mtype === "video")
+                    ctx.telegram.sendVideo(ctx.chat.id, "http://techslides.com/demos/sample-videos/small.mp4")
+
+                //if module type is image
+                else if (moduleResponse[i].mtype === "image")
+                    ctx.telegram.sendPhoto(ctx.chat.id, moduleResponse[i].image)
+
+                ctx.telegram.sendMessage(ctx.chat.id, moduleResponse[i].question, {
+                    reply_markup: {
+                        inline_keyboard: [
+                            [{
+                                'text': moduleResponse[i].option1,
+                                'callback_data': (moduleResponse[i].correctopt === "1") ? "yes" : "no"
+                            }],
+                            [{
+                                'text': moduleResponse[i].option2,
+                                'callback_data': (moduleResponse[i].correctopt === "2") ? "yes" : "no"
+                            }],
+                            [{
+                                'text': moduleResponse[i].option3 ? moduleResponse[i].option3 : "",
+                                'callback_data': (moduleResponse[i].correctopt === "3") ? "yes" : "no"
+                            }],
+                            [{
+                                'text': moduleResponse[i].option4 ? moduleResponse[i].option4 : "",
+                                'callback_data': (moduleResponse[i].correctopt === "4") ? "yes" : "no"
+                            }],
+                        ]
+                    }
+                })
+            }
+
+        });
+
+        telebot.action("no", async (ctx) => {
+            ctx.reply("Opss.. wrong choice please try again")
+            // ctx.replyWithPhoto("https://c.tenor.com/WC2TxVFjEeMAAAAC/mistakes-oops.gif")
+
+        })
+    } else {
+        ctx.telegram.sendMessage(ctx.chat.id, "No Course Module Found")
+    }
+
+});
 
 
 //function definded here !!
 async function getCourses() {
-    let url = "https://bot.creativeknox.com/course";
+    let url = "http://localhost:5000/course";
     try {
         const resp = await axios.get(url);
 
@@ -152,7 +243,8 @@ async function getCourses() {
 
 
 async function getModule(id) {
-    let url = `https://bot.creativeknox.com/module/?cid=${id}`;
+    
+    let url = `http://localhost:5000/module/?cid=${id}`;
     try {
         const resp = await axios.get(url);
 
@@ -166,7 +258,7 @@ async function getModule(id) {
 }
 
 async function startbotserver() {
-    let url = `https://bot.creativeknox.com/`;
+    let url = `http://localhost:5000`;
     try {
         const resp = await axios.get(url);
         if (resp)
